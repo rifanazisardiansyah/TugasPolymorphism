e8edaff0e7b8e3ceea900d8c68f874438f69ebf2
rifanazisardiansyah <101796584+rifanazisardiansyah@users.noreply.github.com>
2022-06-09 23:03:09 +0700

    Add files via upload

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    internal class PrinterWindows
+    {
+        public string PrinterName { get; set; }
+        public virtual void Show()
+        {
+
+        }
+        public virtual void Print()
+        {
+
+        }
+    }
+}
